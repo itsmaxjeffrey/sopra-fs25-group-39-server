b4d990993df0d4962b316b82cf3b00e158f88391
Max Jeffrey <maxjeffrey@protonmail.ch>
2025-03-28 17:08:29 +0100

    fixbug: car-driver one-one entity

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Contract.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Contract.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -13,7 +12,6 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -101,6 +99,7 @@ public class Contract implements Serializable{
 
     @OneToOne
     @JoinColumn(name = "accepted_offer_id")
+    private Offer acceptedOffer;
     
     public Requester getRequester() {
         return this.requester;
