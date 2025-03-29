64f0a54769554454f00773b5aae0bc37a1c1cf5c
dariohug <d.hugwagner@gmail.com>
2025-03-29 01:48:26 +0100

    Uniqueness test passing testcases and testUserBuilder functionality

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/UserServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/UserServiceIntegrationTest.java
@@ -1,20 +1,27 @@
 package ch.uzh.ifi.hase.soprafs24.service;
 
-import ch.uzh.ifi.hase.soprafs24.entity.User;
-import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.server.ResponseStatusException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ch.uzh.ifi.hase.soprafs24.constant.UserAccountType;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
+import ch.uzh.ifi.hase.soprafs24.util.UserBuilder;
 
 /**
  * Test class for the UserResource REST resource.
- *
+ * Use the UserBuilder to build test Users. 
+ * 
+ * @see UserBuilder
  * @see UserService
  */
 @WebAppConfiguration
@@ -33,43 +40,98 @@ public class UserServiceIntegrationTest {
     userRepository.deleteAll();
   }
 
-  /*@Test
+  @Test
   public void createUser_validInputs_success() {
     // given
     assertNull(userRepository.findByUsername("testUsername"));
 
-    User testUser = new User();
-    testUser.setName("testName");
-    testUser.setUsername("testUsername");
+    User testUser = new UserBuilder().withAccountType(UserAccountType.REQUESTER).build();
 
     // when
-    User createdUser = userService.createUser(testUser);
+    User createdUser = userService.createRequester(testUser);
 
     // then
-    assertEquals(testUser.getId(), createdUser.getId());
-    assertEquals(testUser.getName(), createdUser.getName());
+    assertEquals(testUser.getPhoneNumber(), createdUser.getPhoneNumber());
+    assertEquals(testUser.getEmail(), createdUser.getEmail());
+
+    assertEquals(testUser.getUserId(), createdUser.getUserId());
     assertEquals(testUser.getUsername(), createdUser.getUsername());
-    assertNotNull(createdUser.getToken());
-    assertEquals(UserStatus.OFFLINE, createdUser.getStatus());
+    assertEquals(testUser.getFirstName(), createdUser.getFirstName());
+    assertEquals(testUser.getLastName(), createdUser.getLastName());
+
+    assertEquals(testUser.getUserBio(), createdUser.getUserBio());
+    // assertEquals(UserStatus.OFFLINE, createdUser.getStatus());
   }
 
   @Test
-  public void createUser_duplicateUsername_throwsException() {
+  public void createUser_not_unique_userName_failure() {
+    // given
     assertNull(userRepository.findByUsername("testUsername"));
 
-    User testUser = new User();
-    testUser.setName("testName");
-    testUser.setUsername("testUsername");
-    User createdUser = userService.createUser(testUser);
+    User existingUser = new UserBuilder().withUsername("testUsername").withEmail("test1@example.com").withPhoneNumber("+123456789").build();
+    userService.createRequester(existingUser);
 
-    // attempt to create second user with same username
-    User testUser2 = new User();
+    User duplicateUser = new UserBuilder().withUsername("testUsername").withEmail("test2@example.com").withPhoneNumber("+987654321").build();
 
-    // change the name but forget about the username
-    testUser2.setName("testName2");
-    testUser2.setUsername("testUsername");
+    //when
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> userService.createRequester(duplicateUser));
 
-    // check that an error is thrown
-    assertThrows(ResponseStatusException.class, () -> userService.createUser(testUser2));
-  }*/
+    //then 
+    assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+    assertEquals(exception.getReason(), "The Username provided is not unique. Therefore, the account could not be created!");
+  }  
+  
+  @Test
+  public void createUser_not_unique_MailAdress_failure() {
+    // given
+    assertNull(userRepository.findByUsername("testUsername"));
+
+    User existingUser = new UserBuilder().withUsername("testUsername1").withPhoneNumber("+123456789").build();
+    userService.createRequester(existingUser);
+
+    User duplicateUser = new UserBuilder().withUsername("testUsername2").withPhoneNumber("+987654321").build();
+
+    //when
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> userService.createRequester(duplicateUser));
+
+    //then 
+    assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+    assertEquals(exception.getReason(), "The Mail Adress provided is not unique. Therefore, the account could not be created!");
+  }
+
+  @Test
+  public void createUser_not_unique_PhoneNumber_failure() {
+    // given
+    assertNull(userRepository.findByUsername("testUsername"));
+
+    User existingUser = new UserBuilder().withUsername("testUsername1").withEmail("test1@example.com").build();
+    userService.createRequester(existingUser);
+
+    User duplicateUser = new UserBuilder().withUsername("testUsername2").withEmail("test2@example.com").build();
+
+    //when
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> userService.createRequester(duplicateUser));
+
+    //then 
+    assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+    assertEquals(exception.getReason(), "The Phone Number provided is not unique. Therefore, the account could not be created!");
+  }
+
+  @Test
+  public void createUser_not_unique_Username_MailAdress_PhoneNumber_failure() {
+    // given
+    assertNull(userRepository.findByUsername("testUsername"));
+
+    User existingUser = new UserBuilder().build();
+    userService.createRequester(existingUser);
+
+    User duplicateUser = new UserBuilder().build();
+
+    //when
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> userService.createRequester(duplicateUser));
+
+    //then 
+    assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+    assertEquals(exception.getReason(), "The Username, Mail Adress, Phone Number provided are not unique. Therefore, the account could not be created!");
+  }
 }
---
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/util/UserBuilder.java
@@ -0,0 +1,53 @@
+package ch.uzh.ifi.hase.soprafs24.util;
+
+import java.time.LocalDate;
+
+import ch.uzh.ifi.hase.soprafs24.constant.UserAccountType;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+
+/*
+ * 
+ */
+
+public class UserBuilder {
+    private final User user;
+
+    public UserBuilder() {
+        user = new User();
+        user.setFirstName("Max");
+        user.setLastName("Muster");
+        user.setUsername("MaxMuster03");
+        user.setPassword("Password123");
+        user.setEmail("maxmuster@uzh.ch");
+        user.setUserAccountType(UserAccountType.REQUESTER);
+        user.setBirthDate(LocalDate.of(2003, 10, 23));
+        user.setProfilePicturePath("/images/profile/default.png");
+        user.setWalletBalance(100.0);
+        user.setPhoneNumber("+41774882183");
+        user.setUserBio("Default bio");
+    }
+
+    public UserBuilder withAccountType(UserAccountType accountType) {
+        user.setUserAccountType(accountType);
+        return this;
+    }
+
+    public UserBuilder withUsername(String username) {
+        user.setUsername(username);
+        return this;
+    }
+
+    public UserBuilder withEmail(String email) {
+        user.setEmail(email);
+        return this;
+    }
+
+    public UserBuilder withPhoneNumber(String phoneNumber) {
+        user.setPhoneNumber(phoneNumber);
+        return this;
+    }
+
+    public User build() {
+        return user;
+    }
+}
