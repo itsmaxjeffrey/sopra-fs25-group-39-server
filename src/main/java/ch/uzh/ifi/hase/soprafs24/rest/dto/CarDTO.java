8cc6784b87434efd2a4c71a6990275472cbc054a
Max Jeffrey <maxjeffrey@protonmail.ch>
2025-03-29 13:51:42 +0100

    created filestorage service and defined the DTO objects and adjusted the mapper and user service to work with them + added H2 Console port and file storage to app properties

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/CarDTO.java
@@ -0,0 +1,50 @@
+package ch.uzh.ifi.hase.soprafs24.rest.dto;
+
+public class CarDTO {
+
+    private String carModel;
+    private float space;
+    private float supportedWeight;
+    private boolean isElectric;
+    private String licensePlate;
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public float getSpace() {
+        return space;
+    }
+
+    public void setSpace(float space) {
+        this.space = space;
+    }
+
+    public float getSupportedWeight() {
+        return supportedWeight;
+    }
+
+    public void setSupportedWeight(float supportedWeight) {
+        this.supportedWeight = supportedWeight;
+    }
+
+    public boolean isElectric() {
+        return isElectric;
+    }
+
+    public void setElectric(boolean isElectric) {
+        this.isElectric = isElectric;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+}
