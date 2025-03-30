7a2f2bd4aa90f0e9a0a98a7c9a812f272081dfd1
Felix <95569029+xdecentralix@users.noreply.github.com>
2025-03-30 11:29:25 +0200

    feat: added testing structure for contracts

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/ContractControllerTest.java
@@ -0,0 +1,74 @@
+package ch.uzh.ifi.hase.soprafs24.controller;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Contract;
+import ch.uzh.ifi.hase.soprafs24.entity.Location;
+import ch.uzh.ifi.hase.soprafs24.entity.Requester;
+import ch.uzh.ifi.hase.soprafs24.constant.ContractStatus;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.ContractPostDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.LocationDTO;
+import ch.uzh.ifi.hase.soprafs24.service.ContractService;
+import ch.uzh.ifi.hase.soprafs24.service.LocationService;
+import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * ContractControllerTest
+ * This is a WebMvcTest which allows to test the ContractController i.e. GET/POST
+ * request without actually sending them over the network.
+ * This tests if the ContractController works.
+ */
+@WebMvcTest(ContractController.class)
+public class ContractControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ContractService contractService;
+
+    @MockBean
+    private LocationService locationService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    /**
+     * Helper Method to convert contractPostDTO into a JSON string such that the input
+     * can be processed
+     * Input will look like this: {"title": "Test Contract", "mass": 10.0, ...}
+     * 
+     * @param object
+     * @return string
+     */
+    private String asJsonString(final Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                String.format("The request body could not be created.%s", e.toString()));
+        }
+    }
+} 
---
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/repository/ContractRepositoryIntegrationTest.java
@@ -0,0 +1,10 @@
+package ch.uzh.ifi.hase.soprafs24.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@WebAppConfiguration
+@DataJpaTest
+public class ContractRepositoryIntegrationTest {
+    // Test class structure maintained for future tests
+} 
