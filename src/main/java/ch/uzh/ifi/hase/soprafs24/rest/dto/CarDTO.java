fb10ea0510b4b9db9ff73f0700332e3e7f8e9da6
Max Jeffrey <maxjeffrey@protonmail.ch>
2025-03-30 15:59:28 +0200

    setter/getter with lambok in carDTO

--- src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/CarDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/CarDTO.java
@@ -1,5 +1,9 @@
 package ch.uzh.ifi.hase.soprafs24.rest.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class CarDTO {
 
     private String carModel;
@@ -8,43 +12,5 @@ public class CarDTO {
     private boolean isElectric;
     private String licensePlate;
 
-    public String getCarModel() {
-        return carModel;
-    }
 
-    public void setCarModel(String carModel) {
-        this.carModel = carModel;
-    }
-
-    public float getSpace() {
-        return space;
-    }
-
-    public void setSpace(float space) {
-        this.space = space;
-    }
-
-    public float getSupportedWeight() {
-        return supportedWeight;
-    }
-
-    public void setSupportedWeight(float supportedWeight) {
-        this.supportedWeight = supportedWeight;
-    }
-
-    public boolean getIsElectric() {
-        return isElectric;
-    }
-
-    public void setIsElectric(boolean isElectric) {
-        this.isElectric = isElectric;
-    }
-
-    public String getLicensePlate() {
-        return licensePlate;
-    }
-
-    public void setLicensePlate(String licensePlate) {
-        this.licensePlate = licensePlate;
-    }
 }
