1d1f7caa681fca25de83d6e14282e8542a55217f
Felix <95569029+xdecentralix@users.noreply.github.com>
2025-04-18 13:45:44 +0200

    feat: added LocationTest

--- src/test/java/ch/uzh/ifi/hase/soprafs24/entity/RequesterTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/entity/RequesterTest.java
@@ -2,9 +2,6 @@ package ch.uzh.ifi.hase.soprafs24.entity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
