5388ded91b97c82a190b2e9b077d500841f8cf65
Felix <95569029+xdecentralix@users.noreply.github.com>
2025-04-18 15:00:18 +0200

    fix: using constructor injection instead of field injection

--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/GoogleMapsConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/GoogleMapsConfig.java
@@ -3,24 +3,24 @@ package ch.uzh.ifi.hase.soprafs24.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
 @PropertySource(value = "file:.env.local", ignoreResourceNotFound = true)
 public class GoogleMapsConfig {
-
     private static final Logger log = LoggerFactory.getLogger(GoogleMapsConfig.class);
+    
+    private final Environment environment;
 
-    @Autowired
-    private Environment environment;
+    public GoogleMapsConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public String googleMapsApiKey() {
