dc4130af46251d070bf794db0138cd560cdca4db
Max Jeffrey <maxjeffrey@protonmail.ch>
2025-05-07 18:39:48 +0200

    added endpoint to check for for username availability

--- src/main/java/ch/uzh/ifi/hase/soprafs24/security/registration/controller/RegistrationController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/security/registration/controller/RegistrationController.java
@@ -2,6 +2,8 @@ package ch.uzh.ifi.hase.soprafs24.security.registration.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,7 +60,17 @@ public class RegistrationController {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-        /**
+    /**
+     * Check if a username is available
+     * GET /api/v1/auth/check-username/{username}
+     */
+    @GetMapping("/check-username/{username}")
+    public ResponseEntity<Object> checkUsernameAvailability(@PathVariable String username) {
+        boolean isTaken = userRegistrationService.checkUsernameAvailability(username);
+        return ResponseEntity.ok(Map.of("isTaken", isTaken));
+    }
+
+    /**
      * Helper method to create authenticated user response 
      * that includes authentication token and user account type
      */
